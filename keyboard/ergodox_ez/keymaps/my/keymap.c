786f9cf4963e2bd4d22ba1d71a3c72f315320281
Shinji Tanaka <shinji.tanaka@gmail.com>
2016-04-28 17:46:15 +0900

    fix

--- keyboard/ergodox_ez/keymaps/my/keymap.c
+++ keyboard/ergodox_ez/keymaps/my/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TO(OSX,1),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
-        KC_ESC,LALT(KC_DOWN),  LALT(KC_UP),   KC_LALT,KC_LGUI,
+ KC_ESC,LSFT(LALT(KC_DOWN)),LSFT(LALT(KC_UP)),KC_LALT,KC_LGUI,
                                                KC_LEFT,       KC_RGHT,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
