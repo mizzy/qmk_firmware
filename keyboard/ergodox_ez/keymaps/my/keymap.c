8996f7758fc955710c28ddd833517ad4d6773da2
Shinji Tanaka <shinji.tanaka@gmail.com>
2016-04-28 13:09:33 +0900

    fix

--- keyboard/ergodox_ez/keymaps/my/keymap.c
+++ keyboard/ergodox_ez/keymaps/my/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
         TO(SYMB,1),  KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-        TO(MEDIA),   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+        TO(MEDIA,1), KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                           KC_LGUI,   KC_LBRC,KC_RBRC,KC_EQL,           MO(MEDIA),
         KC_DOWN,         KC_UP,
         KC_PGUP,
